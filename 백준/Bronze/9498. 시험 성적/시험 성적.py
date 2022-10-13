2fdef842c85ddb86bfa68ae4e5ce57dc0578d05b
Chaerim Park <115690805+crp9428@users.noreply.github.com>
2022-10-13 21:28:57 +0900

    [Bronze V] Title: 시험 성적, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+jumsu = int(input().strip())
+if jumsu >= 90:
+    print("A")
+elif jumsu < 90 and jumsu >= 80:
+    print("B")
+elif jumsu < 80 and jumsu >= 70:
+    print("C")
+elif jumsu < 70 and jumsu >= 60:
+    print("D")
+else:
+    print("F")
+        
