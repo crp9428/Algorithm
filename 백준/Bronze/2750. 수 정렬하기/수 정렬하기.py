23d38f0f8783633fc95dd9e3539581e160167da5
Chaerim Park <115690805+crp9428@users.noreply.github.com>
2022-10-14 00:38:33 +0900

    Feat: 버블 정렬 코드 추가

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -5,16 +5,26 @@ print = sys.stdout.write
 N = int(input().strip())
 
 # 1. 삽입 정렬
-arr = [int(input().strip())]
-for _ in range(N - 1):
-    num = int(input().strip())
-    for i, v in enumerate(arr):
-        if num < v:
-            arr.insert(i, num)
-            break
-        if i + 1 == len(arr):
-            arr.append(num)
-            break
+# arr = [int(input().strip())]
+# for _ in range(N - 1):
+#     num = int(input().strip())
+#     for i, v in enumerate(arr):
+#         if num < v:
+#             arr.insert(i, num)
+#             break
+#         if i + 1 == len(arr):
+#             arr.append(num)
+#             break
+
+# for v in arr:
+#     print(f"{v}\n")
+    
+# 2. 버블 정렬
+arr = [int(input().strip()) for _ in range(N)]
+for i in range(len(arr) - 1, -1, -1):
+    for j in range(0, i):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 for v in arr:
     print(f"{v}\n")
