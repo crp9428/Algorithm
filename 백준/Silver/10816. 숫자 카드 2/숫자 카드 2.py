0aa9668ced6300c7b417d03ce79a9c726ba6d721
Chaerim Park <115690805+crp9428@users.noreply.github.com>
2022-10-14 21:24:52 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 752 ms, Memory: 127872 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -3,10 +3,10 @@ input = sys.stdin.readline
 print = sys.stdout.write
 
 N = int(input().strip())
-cards = list(0 for _ in range(20000001))
+cards = dict()
 for n in list(map(int, input().strip().split())):
-    cards[n] += 1
-
+    cards[n] = cards[n] + 1 if n in cards else 1
+        
 M = int(input().strip())
 for q in list(map(int, input().strip().split())):
-    print("%d " % cards[q])
+    print("%d " % cards[q]) if q in cards else print("0 ")
